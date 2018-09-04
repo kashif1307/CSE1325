c7204debbad36c8ab8a057f4c724eb6f616554d2
kashif1307 <42795815+kashif1307@users.noreply.github.com>
2018-09-03 19:19:25 -0500

    Bonus git commits

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 
 using namespace std;
 
-
 int main()
 {
   Polygon polygon;
   double temp;
+  int count = 0;
   while (1)
     {
+      count++;
+      if (count > 10){
+	cout<<"You have exceeded the array limit "<< endl;
+	break;
+      }
       cout<< "Please enter a length of a new side"<<endl;
       cin>>temp;
 
@@ -21,9 +26,17 @@ int main()
 	polygon.add_side(temp);
       }
     }
-							      
+  
+   cout<< "Please enter the value of the apothem" <<endl;
+    double ap;
+   cin>>ap;
+   if(ap>0)
+   {
+    cout<<"The area of the regular polygon is "<< polygon.get_area(ap)<<endl;
+   }
   cout << "Num of sides is " << polygon.get_sides() << endl;
   cout << "Perimeter is " << polygon.get_perimeter() << endl;
+  
 }
 	  
 						
--- polygon.cpp
+++ polygon.cpp
@@ -2,27 +2,40 @@
 
 using namespace std;
 
-
 class Polygon{
 
 private:
   double perimeter;
   int sides;
+  double lengths[10];
 
 public:
-  Polygon()
+  Polygon():perimeter(0), sides(0)
   {
-    perimeter = 0;
-    sides = 0;
+      int i;
+      for(i=0;i<10;i++)
+	{
+	  lengths[i]=0;
+	}
   }
+
+ 
+  
   void add_side(double length)
   {
-    perimeter+=length;
+    //    sides=0;
+    lengths[sides]=length;
     sides++;
   }
 
   double get_perimeter()
   {
+    int i;
+    perimeter = 0;
+    for(i=0;i<10;i++)
+      {
+	perimeter += lengths[i];
+      }
     return perimeter;
   }
 
@@ -31,4 +44,15 @@ public:
     return sides;    
   }
   
-};
+
+  double get_area(double apothem)
+  {
+    double area= 0.5 * get_perimeter() * apothem;
+   
+    return area;
+    
+  }
+
+ };
+  
+
